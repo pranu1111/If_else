48826c71db35c3e6405c49c9bfe772cb7a3f2615
Pranita kamble <107885176+pranu1111@users.noreply.github.com>
2023-10-17 10:45:09 +0530

    Add files via upload

---
+++ if_else_elif_ladder.py
@@ -0,0 +1,85 @@
+#a = 10
+#b = 25
+#if a > b:
+#   print("a is greater number")
+#elif a == b:
+#   print("a and b is equal numbers")
+#else:
+#   print("b is greater")   
+
+   
+#c = int(input("enter c's value"))
+#d = int(input("enter d's value"))
+#if c > d:
+#   print("c is greater")
+#elif c == d:
+#   print("c equals to d") 
+#else:
+#   print("d is greater")        
+
+#a = 2
+#b = 330
+#print("A") if a > b else print("B")
+#print("Prani") if a > b else print("Aks")
+
+#a = 222
+#b = 111
+#c = 333
+#if a > b and c > a :
+#   print("both conditions are true")
+#   
+#if a > b or c > b:
+#   print("either one of them is true")
+
+#a = 222
+#b = 222
+#c = 555
+#d = 566
+#if a > b :
+#   print("a is greater")
+#elif a > c:
+#   print("a is greater than c too")
+#elif a > d:
+#   print("a is greater than d too")
+#elif b > a:
+#   print("b is greater")
+#elif b > c:
+#   print("b is greater than c")
+#else:
+#   print("d is greater")
+   
+#a = 33
+#b = 200
+#if not a > b:
+#  print("a is NOT greater than b")
+
+#c = 6
+#d = 5
+#if not c > d:
+#   print("c is not greater")
+#else:
+#   print("d is not greater")
+
+#x = 41
+#if x > 10:
+#  print("Above ten,")
+#  if x > 20:
+#    print("and also above 20!")
+#  else:
+#    print("but not above 20.")
+
+
+#y = 60    
+#if y > 30:
+#   print(" y is greater than 30")
+#   if y > 40:
+#      print("y is greater than 40 too")
+#   else:
+#      print("y is not greater than 40")
+#else:
+#   print("y is not greater than 30 ")   
+
+
+a = 10
+if a > 10:
+   pass
